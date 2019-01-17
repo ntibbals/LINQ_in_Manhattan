9b09c301c427ca278442a3bdbec83d3368675f78
ntibbals <ntibbals@gmail.com>
2019-01-17 11:41:10 -0800

    setup project

---
+++ Lab08-LINQ-Manhattan/Lab08-LINQ-Manhattan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab08_LINQ_Manhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
