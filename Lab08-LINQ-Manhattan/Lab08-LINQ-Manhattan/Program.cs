cecbf20e2eceeec1efdcc94d33fa5c322590c13e
ntibbals <ntibbals@gmail.com>
2019-01-17 12:25:03 -0800

    working on reading file

--- Lab08-LINQ-Manhattan/Lab08-LINQ-Manhattan/Program.cs
+++ Lab08-LINQ-Manhattan/Lab08-LINQ-Manhattan/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Lab08_LINQ_Manhattan
 {
     class Program
     {
+        public static string Path = "../../../../../data.json";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ReadJson();
             
+        }
+
+        static void ReadJson()
+        {
+            JObject cityObject = JObject.Parse(File.ReadAllText(Path));
+            using (StreamReader file = File.OpenText(Path));
+
         }
     }
 }
