3f0500ec83ef953369957321ab7955906f20e44c
ntibbals <ntibbals@gmail.com>
2019-01-17 17:20:31 -0800

    successfully reading in file, can write neighborhoods

--- Lab08-LINQ-Manhattan/Lab08-LINQ-Manhattan/Program.cs
+++ Lab08-LINQ-Manhattan/Lab08-LINQ-Manhattan/Program.cs
@@ -2,6 +2,8 @@
 using Newtonsoft.Json.Linq;
 using System.IO;
 using Newtonsoft.Json;
+using Lab08_LINQ_Manhattan.Classes;
+using System.Collections.Generic;
 
 namespace Lab08_LINQ_Manhattan
 {
@@ -15,10 +17,36 @@ namespace Lab08_LINQ_Manhattan
             
         }
 
-        static void ReadJson()
+        public static List<Properties> ReadJson()
         {
-            JObject cityObject = JObject.Parse(File.ReadAllText(@"../../../../../data.json"));
-            Console.WriteLine(cityObject);           
+            JObject propObject = JObject.Parse(File.ReadAllText(@"../../../../../data.json"));
+            Console.WriteLine(propObject);
+            var newObject = propObject["features"];
+            List<Properties> listProps = new List<Properties>();
+            foreach (var p in newObject)
+            {
+
+
+                Properties props = new Properties
+                {
+                    Zip = (string)p["properties"]["zip"],
+                    City = (string)p["properties"]["city"],
+                    State = (string)p["properties"]["state"],
+                    Address = (string)p["properties"]["address"],
+                    Borough = (string)p["properties"]["borough"],
+                    Neighborhood = (string)p["properties"]["neighborhood"],
+                    County = (string)p["properties"]["county"],
+                };
+                listProps.Add(props);
+            }
+            foreach (Properties item in listProps)
+            {
+                if (item != null)
+                {
+                    Console.Write(item.Neighborhood);
+                }
+            }
+            return listProps;
         }
     }
 }
